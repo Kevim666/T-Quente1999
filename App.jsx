aca2ac5b1e0332f733d007b63b7768f795252a00
Kevim666 <Kevimu252@gmail.com>
2025-05-16 12:01:21 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-red-500 text-white text-4xl font-bold">
+      TáQuente! Site funcionando com Vite + React + Tailwind!
+    </div>
+  )
+}
+
+export default App
