9ccb27d8e2edc860530e614fd50448187ec091d4
Kevim666 <Kevimu252@gmail.com>
2025-05-16 12:09:45 -0300

    Delete App.jsx

--- App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-red-500 text-white text-4xl font-bold">
-      TáQuente! Site funcionando com Vite + React + Tailwind!
-    </div>
-  )
-}
-
-export default App
